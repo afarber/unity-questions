e0b82a4aa57f1ded244c77c87399e099c042fae4
Alexander Farber <farber72@outlook.de>
2022-03-12 17:43:43 +0100

    Implement vertical rotation

--- Walls/Assets/MouseLook.cs
+++ Walls/Assets/MouseLook.cs
@@ -12,16 +12,26 @@ public class MouseLook : MonoBehaviour
     }
 
     public RotationAxes axes = RotationAxes.MouseXAndY;
- 
+    public float sensitivityHor = 10f;
+    public float sensitivityVert = 10f;
+    public float minimumVert = -40f;
+    public float maximumVert = 40f;
+    private float verticalRot = 0;
+
     void Update()
     {
         if (axes == RotationAxes.MouseX)
         {
-            // horizontal rotation here
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
         }
         else if (axes == RotationAxes.MouseY)
         {
-            // vertical rotation here
+            //transform.Rotate(Input.GetAxis("Mouse Y") * sensitivityVer, 0, 0);
+
+            verticalRot -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            verticalRot = Mathf.Clamp(verticalRot, minimumVert, maximumVert);
+            float horizontalRot = transform.localEulerAngles.y; 
+            transform.localEulerAngles = new Vector3(verticalRot, horizontalRot, 0);
         }
         else
         {
