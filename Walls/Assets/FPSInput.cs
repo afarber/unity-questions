39458558516ff1f157423e31d00e98b027ad89c6
Alexander Farber <farber72@outlook.de>
2022-03-13 11:33:14 +0100

    Add collision detection (and FPSInput.cs)

---
+++ Walls/Assets/FPSInput.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSInput : MonoBehaviour
+{
+    private CharacterController charController;
+    public float speed = 1f;
+
+    private void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+  
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        charController.Move(movement);
+    }
+}
